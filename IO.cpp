18a5b5933e7e00d0fb1a5108081696fe616d0481
Alexandru Tifrea <atifrea@mozilla.com>
2015-02-17 13:23:55 +0200

    Added functions for reading the reviews

---
+++ IO.cpp
@@ -0,0 +1,35 @@
+#include <sstream>
+#include "IO.h"
+#include <string>
+
+IO::IO(std::string _in_name, std::string _out_name) {
+  this->in_name = _in_name;
+  this->out_name = _out_name;
+}
+
+
+void IO::readReviews(std::map<std::string, int> frequency,
+                     std::vector<std::vector<std::string> > reviews) {
+  int nr_reviews, i = 0;
+  this->in >> nr_reviews;
+  std::string line;
+  while (i < nr_reviews && std::getline(this->in, line)) {
+    // Separator between two consecutive reviews
+    if (line.compare("-----") == 0) {
+      i++;
+      continue;
+    }
+
+    // Update hashtable
+    std::istringstream iss(line);
+    std::string word;
+    std::vector<std::string> newWords;
+    while (iss >> word) {
+      frequency[word]++;
+      newWords.push_back(word);
+    }
+
+    // Update the array
+    reviews[i].insert(reviews[i].end(), newWords.begin(), newWords.end());
+  }
+}
---
+++ IO.h
@@ -0,0 +1,28 @@
+#ifndef __IO_H__
+#define __IO_H__
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+class IO {
+private:
+  std::string in_name, out_name;
+  std::ifstream in;
+  std::ofstream out;
+
+public:
+  IO(std::string _in_name, std::string _out_name);
+
+  ~IO() {}
+
+  /*
+   * Read reviews from file.
+   */
+  void readReviews(std::map<std::string, int> frequency,
+                   std::vector<std::vector<std::string> > reviews);
+};
+
+#endif // __IO_H__
