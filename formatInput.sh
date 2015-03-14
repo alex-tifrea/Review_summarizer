caf57b9f9ff1bb22b317bb8958b7f90dedbe53cf
Alexandru Tifrea <alex.tifrea93@gmail.com>
2015-03-14 12:36:39 +0200

    formatInput now working smoothly

--- formatInput.sh
+++ formatInput.sh
@@ -3,7 +3,8 @@
 # python xml_input.py -f "Review_Bucharest_Hotel_Christina"
 # old_reviews=cat reviews.in
 # TODO: sterge linia asta
-old_reviews=$(cat input_example)
-echo $old_reviews
-formatted_reviews=${old_reviews/"."/" . "}
-echo $formatted_reviews > tmp
+old_reviews="$(cat input_example)"
+echo "$old_reviews"
+
+formatted_reviews="${old_reviews/"."/" . "}"
+echo "$formatted_reviews" > tmp
