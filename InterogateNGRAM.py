ac803510710dc4287c5a6f2dc5ec3ac0f0398a9a
Mihai Volmer <mihaivolmer@gmail.com>
2015-03-30 21:43:27 +0300

    Fixed interogate crashing when the html request is too long

--- InterogateNGRAM.py
+++ InterogateNGRAM.py
@@ -4,6 +4,7 @@ import MicrosoftNgram
 import sys, io, os, time
 
 E_VALUE = 2.71828
+MAX_LENGTH = 55000
 
 lookup = MicrosoftNgram.LookupService()
 
@@ -19,14 +20,10 @@ out_deb = open("InterogateNGRAM.out", "w", 0)
 out_deb.write('am deschis iesirea\n')
 #print 'py am deschis iesirea\n'
 
-#requestInput = open("/tmp/ngramfiforeq", "r")
-#out_deb.write('am deschis requestInput\n')
-
-#out = open("/tmp/ngramfifo", "w", 0);
-#out_deb.write('py am deschis output\n')
-
 buffer = []
 buffering = False
+buffer_len = 0
+result = ""
 
 while (1):
     #print 'py astept sa primesc ceva de la requestInput'
@@ -44,27 +41,49 @@ while (1):
             out_deb.write('Incep sa fac buffer\n')
             buffer = []
             buffering = True
+            buffer_len = 0
+            result = ""
             continue
         elif (line == 'Interogate please send queries\n'):
-            out_deb.write('Am terminat de facut buffer\n')
-            result = lookup.GetJointProbabilities(buffer)
+            #out_deb.write('Am terminat de facut buffer\n')
+            """
+            Check if we have previous results
+            """
+            if result != "":
+                result += '\n'
+            if buffer != []:
+                result += lookup.GetJointProbabilities(buffer)
             result_final = ""
             for nr in result.split('\n'):
                 result_final = result_final + str(E_VALUE**(float(nr))) + "\n"
-            out_deb.write("result " + result_final)
+            #out_deb.write("result " + result_final)
             resultOutput.write(result_final)
             resultOutput.flush()
-            out_deb.write('am trimis rezultat final\n')
+            #out_deb.write('am trimis rezultat final\n')
             buffer = []
             buffering = False
+            buffer_len = 0
+            result = ""
             continue
 
         if buffering:
             '''remove the '\n' from the end of the line'''
             buffer.append(line[:-1])
+            buffer_len += len(line[:-1])
+            #out_deb.write("dimensiunea bufferului: " + str(buffer_len) + "\n")
+            if buffer_len > MAX_LENGTH:
+                out_deb.write("am atins dimensiunea maxima, fac req\n")
+                """
+                Reached maximum size of html requests
+                """
+                if result != "":
+                    result += '\n'
+                result += lookup.GetJointProbabilities(buffer)
+                buffer = []
+                buffer_len = 0
             continue
         result = str(E_VALUE**lookup.GetJointProbability(line))
-        out_deb.write('am calculat rezultatul ' + result + ' pentru ' + line + '\n')
+        #out_deb.write('am calculat rezultatul ' + result + ' pentru ' + line + '\n')
         resultOutput.write(result + '\n')
         resultOutput.flush()
     else:
