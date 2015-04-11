9eb79c588f51c61d4e6362412d5fd90b539ff32a
Alexandru Tifrea <alex.tifrea93@gmail.com>
2015-04-11 22:27:25 +0300

    Fixed bug in refineNgram

--- POS.cpp
+++ POS.cpp
@@ -2,7 +2,7 @@
 
 const std::vector<std::string> POS_t::name = {
         "CC", "CD", "DT", "EX", "FW", "IN", "JJ", "JJR", "JJS", "LS", "MD",
-        "NN", "NNS", "NNP", "NNPS", "PDT", /*"POS",*/ "PRP", "PRP$", "RB", "RBR",
+        "NN", "NNS", "NNP", "NNPS", "PDT", "POS", "PRP", "PRP$", "RB", "RBR",
         "RBS", "RP", "SYM", "TO", "UH", "VB", "VBD", "VBG", "VBN", "VBP",
         "VBZ", "WDT", "WP", "WP$", "WRB"
     };
