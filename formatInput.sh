e3628a38f3eeade07eecc25c5f1679a1be47e022
Alexandru Tifrea <alex.tifrea93@gmail.com>
2015-05-13 18:46:53 +0300

    changes to formatinput

--- formatInput.sh
+++ formatInput.sh
@@ -3,12 +3,8 @@
 # Converts all words to lowercase.
 # @args the file which need to be formatted and the destination file
 
-# TODO: decomenteaza liniile de jos
-# python xml_input.py -f "Review_Bucharest_Hotel_Christina"
-# old_reviews=cat reviews.in
-# TODO: sterge linia asta
-input=$1
-old_reviews="$(cat $input)"
+python xml_input.py -f $1
+old_reviews=$(cat reviews.in)
 
 formatted_reviews=$(echo "$old_reviews" | tr -d "():;,\"'[]{}*&%\$\#@~\`\\></=+_-")
 formatted_reviews=$(echo "$formatted_reviews" | tr -s " ")
